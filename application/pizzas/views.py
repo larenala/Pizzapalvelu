f3ad86f7e4950b11cdaee02e9f82118353c98db6
Laura <laura.reivinen@gmail.com>
2019-01-31 15:40:03 +0200

    added styles

--- application/pizzas/views.py
+++ application/pizzas/views.py
@@ -1,20 +1,32 @@
 from application import app, db
+
 from flask import redirect, render_template, request, url_for
+from flask_login import login_required
+
 from application.pizzas.models import Pizza
+from application.pizzas.forms import PizzaForm
 
 @app.route("/pizzas", methods=["GET"])
 def pizzas_index():
    return render_template("pizzas/list.html", pizzas=Pizza.query.all()) 
 
 @app.route("/pizzas/new/")
+@login_required
 def pizzas_form():
-    return render_template("pizzas/new.html")
+    return render_template("pizzas/new.html", form=PizzaForm())
 
 @app.route("/pizzas/", methods=["POST"])
+@login_required
 def pizza_create():
-    name = request.form.get("name")
-    ingredient = request.form.get("ingredient")
-    p = Pizza(name, ingredient)
+    form = PizzaForm(request.form)
+
+    if not form.validate():
+        return render_template("pizzas/new.html", form = form)
+
+    name = form.name.data
+    ingredients = form.ingredients.data
+    img = form.img.data
+    p = Pizza(name, ingredients, img)
     db.session().add(p)
     db.session().commit()  
     return redirect(url_for('pizzas_index'))
