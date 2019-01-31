215cfdc127d6869a2dbad4fd82371d5f55d6f0dc
Laura <laura.reivinen@gmail.com>
2019-01-31 15:39:42 +0200

    added image

--- application/pizzas/models.py
+++ application/pizzas/models.py
@@ -3,9 +3,11 @@ from application import db
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     onupdate=db.func.current_timestamp()
-    ingredient = db.Column(db.String(144), nullable=False)
+    ingredients = db.Column(db.String(144), nullable=False)
     name = db.Column(db.String(144), nullable=False)
+    img = db.Column(db.String(200), nullable=False)
 
-    def __init__(self, name, ingredient):
+    def __init__(self, name, ingredients, img):
         self.name = name
-        self.ingredient = ingredient
+        self.ingredients = ingredients
+        self.img = img
