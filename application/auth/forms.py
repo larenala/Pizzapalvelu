3e1c9d5a148e9e7cb397242a2718ef221c36eaf9
Laura <laura.reivinen@gmail.com>
2019-02-07 10:38:29 +0200

    added validation for password

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -11,7 +11,7 @@ class LoginForm(FlaskForm):
 class AccountForm(FlaskForm):
      name = StringField("Name")
      username = StringField("Username")
-     password = PasswordField("Password")
+     password = PasswordField("Password", [validators.Length(min=8, max=25)])
 
      class Meta:
          csrf = False
