4b63faa05fd893dc0195c3dbbbca94cbb8d47ad1
Laura <laura.reivinen@gmail.com>
2019-02-08 18:38:07 +0200

    added postgre

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,15 @@ app = Flask(__name__)
 # Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orders.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzas.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orderpizzas.db"
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orders.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzas.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///accounts.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///orderpizzas.db"
 
 # Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
 app.config["SQLALCHEMY_ECHO"] = True
@@ -45,4 +50,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan lopulta tarvittavat tietokantataulut
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
