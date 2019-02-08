190628c192914c8d6f339bd50de5b0ff98d325bc
Laura <laura.reivinen@gmail.com>
2019-02-08 19:55:37 +0200

    added more validators

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,16 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
+    username = StringField("Username", [validators.InputRequired])
+    password = PasswordField("Password", [validators.InputRequired("Please enter your password.")])
   
     class Meta:
         csrf = False
 
 class AccountForm(FlaskForm):
-     name = StringField("Name")
-     username = StringField("Username")
+     name = StringField("Name", [validators.InputRequired("Please enter your name.")])
+     username = StringField("Username", [validators.Length(min=2, max=25)])
      password = PasswordField("Password", [validators.Length(min=8, max=25)])
+     password = PasswordField('New Password', [
+         validators.DataRequired(),
+         validators.EqualTo('confirm', message='Passwords must match')
+     ])
+     confirm = PasswordField('Repeat Password')
 
      class Meta:
          csrf = False
--- application/orders/forms.py
+++ application/orders/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, validators
 
 class OrderForm(FlaskForm):
-    name = StringField("Name")
-    address = StringField("Delivery address")
-    phone = StringField("Phone number")
+    name = StringField("Name", [validators.Length(min=4, max=35)])
+    address = StringField("Delivery address", [validators.Length(min=10, max=50)])
+    phone = StringField("Phone number", [validators.Length(min=6, max=15)])
 
     class Meta:
         csrf = False
