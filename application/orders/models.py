26dc26f3e058a9bf1fd8d013a67ac4c971259708
Laura <laura.reivinen@gmail.com>
2019-02-08 18:09:05 +0200

    added relation

--- application/orders/models.py
+++ application/orders/models.py
@@ -1,6 +1,11 @@
 from application import db
 
+from sqlalchemy.sql import text
+
 class Tilaus(db.Model):
+
+    __tablename__ = "order"
+
     id = db.Column(db.Integer, primary_key=True)
     order_date = db.Column(db.DateTime, default=db.func.current_timestamp())
     delivery_date = db.Column(db.DateTime, default=db.func.current_timestamp(),
@@ -10,11 +15,34 @@ class Tilaus(db.Model):
     phone = db.Column(db.String(15))
     delivered = db.Column(db.Boolean, nullable=False)
     price = db.Column(db.String(20), nullable=False)
+    items = db.Column(db.String(100))
+
+    orderedPizzas=db.relationship("OrderPizza", backref='order', lazy=True)
 
     account_id=db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
+    
 
     def __init__(self, account_id, price=0):
         self.account_id = account_id
         self.delivered = False
-        self.price = price
+        self.price = price 
+        self.items = ""
+
+
+class OrderPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id=db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    pizza_id=db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
+
+    def __init__(self, order_id, pizza_id):
+        self.order_id = order_id    
+        self.pizza_id = pizza_id
+
+    @staticmethod
+    def find_pizza():
+        stmt=text("SELECT pizza.id, pizza.name, pizza.price FROM Pizza, OrderPizza WHERE pizza.id=OrderPizza.pizza_id")
+        res=db.engine.execute(stmt)
+
+        for row in res:
+            print(row[0])
--- application/orders/views.py
+++ application/orders/views.py
@@ -1,14 +1,22 @@
 
 from application import app, db
 from flask import redirect, render_template, request, url_for
-from application.orders.models import Tilaus
+from application.orders.models import Tilaus, OrderPizza
 from application.orders.forms import OrderForm
+from application.pizzas.models import Pizza
 from application.auth.models import User
+
 from flask_login import login_required, current_user
 
 @app.route("/orders/", methods=["GET"])
 def orders_index():
     return render_template("orders/list.html", orders=Tilaus.query.all())
+
+
+@app.route("/myorders/", methods=["GET"])
+def myorders_index():
+    id = current_user.get_id()
+    return render_template("orders/myorders.html", orders=Tilaus.query.filter_by(account_id=id))
 
 @app.route("/orders/<order_id>/", methods=["POST"])
 def orders_set_delivered(order_id):
@@ -18,12 +26,25 @@ def orders_set_delivered(order_id):
 
     return redirect(url_for("orders_index"))
 
+@app.route("/orders/<order_id>", methods=["GET"])
+@login_required
+def show_order(order_id):
+    id=order_id
+    orderPizzas = OrderPizza.query.filter_by(order_id=id)
+    pizzalist = [] 
+    for item in orderPizzas:
+        p_id = item.pizza_id
+        pizza = Pizza.query.get(p_id)
+        pizzalist.append(pizza)
+    return render_template("orders/new.html", form=OrderForm(),  pizzas = pizzalist, order=Tilaus.query.filter_by(id=order_id).first())
+
 @app.route("/orders/new/")
 @login_required
 def orders_form():
     id = current_user.get_id()
     user=User.query.get(id)
     return render_template("orders/new.html", form=OrderForm(), user=user)
+
 
 @app.route("/orders/", methods=["POST"])
 @login_required
@@ -33,12 +54,16 @@ def orders_send():
         return render_template("orders/new.html", form=form)
 
     id = current_user.get_id()
-    price=10
-    t = Tilaus(id, price)
-    t.name=form.name.data
-    t.address=form.address.data
-    t.phone = form.phone.data
-    db.session().add(t)
+    user = user.get_by_id(id)
+    orders = Tilaus.query.filter_by(account_id=id).count()
+    len = orders.count()
+    order = orders[len]    
+    order.price=order.price
+    order.username=form.name.data
+    order.address=form.address.data
+    order.phone = form.phone.data
+    user.current_order = False
+    db.session().add(order)
     db.session().commit()
 
-    return redirect(url_for("orders_index"))
+    return redirect(url_for("myorders_index"))
