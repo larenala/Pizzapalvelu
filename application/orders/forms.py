35306f450b06c1ab9daea72a784aca257ef7375f
Laura <laura.reivinen@gmail.com>
2019-02-28 14:32:55 +0200

    added validators

--- application/orders/forms.py
+++ application/orders/forms.py
@@ -2,10 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class OrderForm(FlaskForm):
-    items = StringField("Ordered items ")
-    name = StringField("Name", [validators.Length(min=4, max=35)])
-    address = StringField("Delivery address", [validators.Length(min=4, max=50)])
-    phone = StringField("Phone number", [validators.Length(min=6, max=15)])
+    name = StringField("Name", [
+        validators.InputRequired("Please enter a name."),
+        validators.Length(min=4, max=35)])
+    address = StringField("Delivery address", [
+        validators.InputRequired("Please enter a delivery address."),
+        validators.Length(min=4, max=80)])
+    phone = StringField("Phone number", [
+        validators.InputRequired("Please enter a phonenumber."),
+        validators.Length(min=6, max=15)])
 
     class Meta:
         csrf = False
--- application/pizzas/forms.py
+++ application/pizzas/forms.py
@@ -2,10 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
 class PizzaForm(FlaskForm):
-    name = StringField("Name ", [validators.Length(min=4, max=30)])
-    ingredients = StringField("Ingredients ", [validators.Length(min=2, max=100)])    
-    img = StringField("Link to image ", [validators.URL(require_tld=False, message=None)]) 
-    price = StringField("Price ", [validators.Length(min=2, max=6)]) 
+    name = StringField("Name ", [
+        validators.InputRequired("Please enter the name of the pizza."),
+        validators.Length(min=4, max=30)])
+    ingredients = StringField("Ingredients ", [
+        validators.InputRequired("Please enter the ingredients."),
+        validators.Length(min=2, max=100)])    
+    img = StringField("Link to image ", [
+        validators.InputRequired("Please enter a valid url for image."),
+        validators.URL(require_tld=False, message="Please enter a valid url")]) 
+    price = StringField("Price ", [
+        validators.InputRequired("Please enter a price."),
+        validators.Length(min=1, max=6)]) 
 
     class Meta:
         csrf = False
